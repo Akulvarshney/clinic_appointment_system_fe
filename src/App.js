697d8212f09ba7058f5297983e7df60a30bf5c1c
siddhant049 <siddhant0009@gmail.com>
2025-07-29 23:53:45 +0530

    initial-setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+}
+
+export default App;
--- src/slices/authSlices/authSlice.js
+++
@@ -1,6 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const authSlice = createSlice({});
-
-export const {} = authSlice.actions;
-export default authSlice.reducer;
